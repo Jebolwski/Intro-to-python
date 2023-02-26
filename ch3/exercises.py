3588c80aa605e0836b42df4dd1d8704245e85cbd
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2023-02-26 12:28:26 +0300

    Add files via upload

--- ch3/exercises.py
+++ ch3/exercises.py
@@ -26,6 +26,7 @@ def threedottwo():
 # ? <<<<<<<<<<<
 
 # !3.4
+
 def threedotfour():
     for i in range(3):
         for j in range(3):
@@ -88,21 +89,21 @@ def threedotten():
         print(wage, i)
 
 
-#!3.11
+# !3.11
 def threedoteleven():
-    rabbitst=0
-    colonyt=0
+    rabbitst = 0
+    colonyt = 0
     while True:
         colony = int(input("Enter the number of does in the rabbit colony (-1 to end) : "))
         if colony == -1:
             break
-        colonyt+=colony
+        colonyt += colony
         rabbits = int(input("Number of baby rabbits born in the past month : "))
-        rabbitst+=rabbits
-    print(rabbitst/colonyt)
+        rabbitst += rabbits
+    print(rabbitst / colonyt)
 
 
-#!3.12
+# !3.12
 def threedottwelve():
     first = int(input("First side : "))
     second = int(input("Second side : "))
@@ -113,3 +114,58 @@ def threedottwelve():
     else:
         print("Not equilateral triangle")
 
+
+# !3.13
+def threedottirtheen():
+    for i in range(1, 100):
+        total = 0
+        for j in range(1, i):
+            if i % j == 0:
+                total += j
+        if total == i:
+            print(i)
+
+
+# !3.14
+def threedotfourteen():
+    flag = True
+    total = 4
+    for i in range(3, 5000, 2):
+        if flag:
+            total -= 4 / i
+            flag = False
+        else:
+            total += 4 / i
+            flag = True
+    print(total)
+
+
+# !3.15
+def threedotfifteen(num):
+    c = 0
+    a = 0
+    b = 1
+    while c < num:
+        c = a + b
+        print(c)
+        a = b
+        b = c
+
+
+# !3.17
+def threedotseventeen():
+
+    for i in range(1, 8):
+        print('*' * i)
+
+    for i in range(8, 0, -1):
+        print('*' * i)
+
+    for i in range(0, 8, 1):
+        print(' ' * i + '*' * (8-i))
+
+    for i in range(0, 8, 1):
+        print(' ' * (8-i) + '*' * i)
+
+
+threedotseventeen()
